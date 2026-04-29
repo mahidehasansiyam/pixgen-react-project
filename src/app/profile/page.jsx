08e70ccf13fbc0218eca4b6a99e46201079a31b2
Mahedi <mahidehasansiyam123@gmail.com>
2026-04-30 00:28:15 +0600

    modify

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -9,20 +9,24 @@ const ProfilePage = () => {
   console.log(session?.user.name, session?.user.image);
 
   return (
-    <div>
-      <Avatar>
-        <Avatar.Image
-          alt="oh"
-          src={session?.user.image}
-          referrerPolicy="no-referrer"
-        >
-          {/* <Avatar.Fallback>
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className=" bg-gray-100 p-8 rounded-2xl flex flex-col gap-3 text-center">
+        <div className='flex justify-center items-center'>
+          <Avatar>
+            <Avatar.Image
+              alt="oh"
+              src={session?.user.image}
+              referrerPolicy="no-referrer"
+            >
+              {/* <Avatar.Fallback>
             {session?.user?.name.charAt(0).toUpperCase()}
           </Avatar.Fallback>  */}
-        </Avatar.Image>
-      </Avatar>
-      <div>{session?.user.email}</div>
-      <ProfileModal></ProfileModal>
+            </Avatar.Image>
+          </Avatar>
+        </div>
+        <div>{session?.user.email}</div>
+        <ProfileModal></ProfileModal>
+      </div>
     </div>
   );
 };
