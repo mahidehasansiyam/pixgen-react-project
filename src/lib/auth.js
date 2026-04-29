030a66c8b1f495b995b5389243d4125dbacc842e
Mahedi <mahidehasansiyam123@gmail.com>
2026-04-29 22:30:57 +0600

    'm'

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,5 @@
-import dns from 'node:dns';
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+// import dns from 'node:dns';
+// dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 import { betterAuth } from 'better-auth';
 import { MongoClient } from 'mongodb';
@@ -14,6 +14,7 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
+      prompt: 'select_account',
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
