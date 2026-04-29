36479c0752a4d01e9af8f06b36122851396e6c33
Mahedi <mahidehasansiyam123@gmail.com>
2026-04-29 19:48:29 +0600

    photo details

--- src/app/photo/[id]/page.jsx
+++ src/app/photo/[id]/page.jsx
@@ -1,19 +1,25 @@
-import { getAllData } from "@/data/page";
-
+import { getAllData } from '@/data/page';
+import Image from 'next/image';
 
 const page = async ({ params }) => {
   const allData = await getAllData();
-    // console.log(allData);
+  // console.log(allData);
 
   const { id } = await params;
   // console.log(id);
 
-  const pic = allData.find((i) =>  id == i.id )
+  const pic = allData.find(i => id == i.id);
   // console.log(pic);
-
+      
   return (
-    <div>
-      nfvdnfv
+    <div className=" mt-8 flex justify-center items-center ">
+      <div className="space-y-4 rounded-2xl p-6 bg-gray-100 text-center ">
+        <div className='flex justify-center items-center'>
+          <Image src={pic.imageUrl} width={300} height={300} alt="ok"></Image>
+        </div>
+        <h2>{pic.title}</h2>
+        <h2>Prompt = {pic.prompt}</h2>
+      </div>
     </div>
   );
 };
